fb648e9dd798b14b8fc7295ae846a0b99c900465
cppljevans <cppljevans@suddenlink.net>
2016-10-19 07:29:32 -0500

    Implement most of soa_block.
    Now code print's out the members in the soa.
    Still need to implement resize.

--- soa_block.cpp
+++ soa_block.cpp
@@ -7,7 +7,8 @@
 #include <iostream>
 #include <array>
 #include <cassert>
-#include <cmath>
+#include <type_traits>
+#include <algorithm>
   std::size_t
 nxt_offset
   ( std::size_t now_offset
@@ -67,62 +68,264 @@ vec_offsets
     return result;
   }
   template
-  < typename... Ts
+  < std::size_t Index
+  , typename T
   >
   struct
-soa_block
-  /**@brief
-   *  An *outline* of how to put all arrays in contiguous block
-   *  of memory (the my_storage member variable below).
-   *  Of course, all the other member functions for
-   *  accessing the "internal subarrays" would need to be added.
-   */
+soa_vec
+  {
+  public:
+      void
+    construct(char* storage, std::size_t size=1)
+      {
+        T* begin=cast(storage);
+        T* end=begin+size;
+        for(T* now=begin; now!=end; ++now)
+          new(now) T;
+      }
+      void
+    destruct(char* storage, std::size_t size=1)
+      {
+        T* begin=cast(storage);
+        T* end=begin+size;
+        for(T* now=begin; now!=end; ++now)
+          now->~T();
+      }
+      T*
+    ptr_at(char* storage, std::size_t index)
+      {
+        return cast(storage)+index;
+      }
+      T const*
+    ptr_at(char const* storage, std::size_t index)const
+      {
+        return cast(storage)+index;
+      }
+  private:      
+      T* 
+    cast(char* storage)
+      {  return static_cast<T*>(static_cast<void*>(storage));
+      }
+      T const* 
+    cast(char const* storage)
+      {  return static_cast<T const*>(static_cast<void const*>(storage));
+      }
+  };
+  template
+  < typename Indices
+  , typename... Ts
+  >
+  struct
+soa_impl
+  ;
+  template
+  < std::size_t... Indices
+  , typename... Ts
+  >
+  struct
+soa_impl
+  < std::integer_sequence< std::size_t, Indices...>
+  , Ts...
+  >
+  : soa_vec<Indices,Ts>... 
   {
   private:
     std::size_t my_vec_size;
     using offsets_t=decltype(vec_offsets<Ts...>(1));
     offsets_t my_offsets;
     char*my_storage;
+      template
+      < std::size_t Index
+      , typename T
+      >
+        static
+      soa_vec<Index,T>&
+    get_self
+      ( soa_vec<Index,T>&self
+      )
+      { return self;
+      }
+      template
+      < std::size_t Index
+      , typename T
+      >
+        static
+      soa_vec<Index,T> const&
+    get_self
+      ( soa_vec<Index,T>const&self
+      )
+      { return self;
+      }
+      template
+      < std::size_t Index
+      >
+      auto&
+    get_vec()
+      { return get_self<Index>(*this);
+      }
+      template
+      < std::size_t Index
+      >
+      auto const&
+    get_vec()const
+      { return get_self<Index>(*this);
+      }
+      char*
+    storage_at(std::size_t index)
+      { return my_storage+my_offsets[index];
+      }
+      char const*
+    storage_at(std::size_t index)const
+      { return my_storage+my_offsets[index];
+      }
+      template
+      < std::size_t Index
+      >
+      void
+    construct
+      (
+      )
+      {
+        get_vec<Index>().construct(storage_at(Index),my_vec_size);
+      }
+      template
+      < std::size_t Index
+      >
+      void
+    destruct
+      (
+      )
+      {
+        get_vec<Index>().destruct(storage_at(Index),my_vec_size);
+      }
+  protected:
+    soa_impl(std::size_t a_vec_size)
+      : my_vec_size(a_vec_size)
+      , my_offsets(vec_offsets<Ts...>(a_vec_size))
+      , my_storage(new char[my_offsets.back()])
+        //IIRC, the alignment of the return from new
+        //is large enough to accommodate any alignment.
+        //Hence, my_offsets.front() can be 0, which
+        //is what is returned by vec_offsets.
+      { 
+        using swallow = int[]; // guaranties left to right order
+        (void)swallow{0, (void(this->template construct<Indices>()), 0)...};
+      }
+      void 
+    resize(std::size_t a_vec_size)
+      { offsets_t resized_offsets(vec_offsets<Ts...>(a_vec_size));
+        char* resized_storage=new char[resized_offsets.back()];
+        //***TODO***
+        //copy my_storage to resized_storage,
+        //Then, initialize resized_storage that has *not*
+        //been copied to, if needed, using, as with
+        //CTOR, some sort of placement new code.
+        my_offsets=resized_offsets;
+        delete[] my_storage;
+        my_storage=resized_storage;
+      }
+    ~soa_impl()
+      {
+        using swallow = int[]; // guaranties left to right order
+        (void)swallow{0, (void(this->template destruct<Indices>()), 0)...};
+        delete[] my_storage;
+      }
   public:
+      template
+      < std::size_t Index
+      >
+      auto*
+    begin()
+      {
+        return get_vec<Index>().ptr_at(storage_at(Index),0);
+      }
+      template
+      < std::size_t Index
+      >
+      auto*
+    end()
+      {
+        return get_vec<Index>().ptr_at(storage_at(Index),my_vec_size);
+      }
+      template
+      < std::size_t Index
+      >
+      auto const*
+    begin()const
+      {
+        return get_vec<Index>().ptr_at(storage_at(Index),0);
+      }
+      template
+      < std::size_t Index
+      >
+      auto const*
+    end()const
+      {
+        return get_vec<Index>().ptr_at(storage_at(Index),my_vec_size);
+      }
+  };
+  template
+  < typename... Ts
+  >
+  struct
+soa_block
+  /**@brief
+   *  Implementation of the single block of storage for
+   *  structure of arrays mentioned in //Purpose comment
+   *  at top of this file.
+   */
+  : public soa_impl< std::index_sequence_for<Ts...>, Ts...>
+  {
+  public:
+    using super_t=soa_impl< std::index_sequence_for<Ts...>, Ts...>;
     soa_block(std::size_t a_vec_size)
-    : my_vec_size(a_vec_size)
-    , my_offsets(vec_offsets<Ts...>(a_vec_size))
-    , my_storage(new char[my_offsets.back()])
-      //IIRC, the alignment of the return from new
-      //is large enough to accommodate any alignment.
-      //Hence, my_offsets.front() can be 0, which
-      //is what is returned by vec_offsets.
+    : super_t(a_vec_size)
     {  
-      //***TODO***
-      //initialize elements of Ts[a_vec_size]...
-      //using some sort of placement new code here.
     }
     void resize(std::size_t a_vec_size)
-    { offsets_t resized_offsets(vec_offsets<Ts...>(a_vec_size));
-      char* resized_storage=new char[resized_offsets.back()];
-      //***TODO***
-      //copy my_storage to resized_storage,
-      //Then, initialize resized_storage that has *not*
-      //been copied to, if needed, using, as with
-      //CTOR, some sort of placement new code.
-      my_offsets=resized_offsets;
-      delete[] my_storage;
-      my_storage=resized_storage;
+    { this->super_t::resize(a_vec_size);
     }
   };
   
+  unsigned
+udt_count=0
+  ;
   template
   < unsigned Id
   >
   struct 
   alignas
-  ( 1<<Id%7 
-    //==pow(2,Id%7).
-    //The c++ standard, IIRC, says all alignments are power of 2.
+  ( std::max
+    ( std::size_t(1<<Id%7)
+      //==pow(2,Id%7).
+      //The c++ standard, IIRC, says all alignments are power of 2.
+    , std::alignment_of<unsigned>::value
+      //account for my_id alignment.
+    )
   ) 
 udt //some User Defined Type.
   {
+    unsigned my_id;
     char c[2*(Id+1)];//Take up some space.
+        friend
+      std::ostream&
+    operator<<
+      ( std::ostream& sout
+      , udt const& x
+      )
+      {
+        return sout<<"udt<"<<Id<<">:my_id="<<x.my_id;
+      }
+    udt()
+      : my_id(udt_count++)
+      {
+        std::cout<<"CTOR(default):"<<*this<<"\n";
+      }
+    udt(udt const&)
+      : my_id(udt_count++)
+      {
+        std::cout<<"CTOR(copy):"<<*this<<"\n";
+      }
   };
   template
   < typename... Ts
@@ -145,6 +348,12 @@ main()
     test_offsets<udt<0>,udt<3>>(vec_size);
     test_offsets<udt<3>,udt<0>>(vec_size);
     soa_block<udt<3>,udt<0>> soa_v(vec_size);
+    unsigned i=0;
+    for(auto* p=soa_v.begin<0>(); p< soa_v.end<0>(); ++p,++i)
+      std::cout<<"p["<<i<<"]="<<*p<<"\n";
+    i=0;
+    for(auto* p=soa_v.begin<1>(); p< soa_v.end<1>(); ++p,++i)
+      std::cout<<"p["<<i<<"]="<<*p<<"\n";
     soa_v.resize(vec_size*2);
     return 0;
   }  
